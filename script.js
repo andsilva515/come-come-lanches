4ab50d2144dbabddf08217cfc7a54a91a090602c
andsilva515 <anderson.ssilva@outlook.com>
2024-10-12 11:20:21 -0300

    criação script js e elementos

---
+++ script.js
@@ -0,0 +1,11 @@
+const menu = document.getElementById("menu")
+const cartBtn = document.getElementById("cart-btn")
+const cartModal = document.getElementById("cart-modal")
+const cartItemsContainer = document.getElementById("cart-items")
+const cartTotal = document.getElementById("cart-total")
+const checkoutBtn = document.getElementById("checkout-btn")
+const closeModalBtn = document.getElementById("close-modal-bn")
+const cartCounter = document.getElementById("cart-count")
+const addressInput = document.getElementById("address")
+const addressWarn = document.getElementById("address-warn")
+
