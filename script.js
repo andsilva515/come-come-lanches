8e117de8068bfd40870b3c412c6d3d6f1cfdf151
andsilva515 <anderson.ssilva@outlook.com>
2024-10-13 14:37:00 -0300

    enviar pedido ap whatsapp

--- script.js
+++ script.js
@@ -133,14 +133,66 @@ addressInput.addEventListener("input", function(event){
 
 
 // Finaliza pedido
-checkoutBtn.addEventListener("click", function(){   
+checkoutBtn.addEventListener("click", function(){  
+    
+    // verifica lanchonete fechada
+    // const isOpen = checkRestaurantOpen();
+    //     if(!isOpen){
+    //     Toastify({
+    //         text: "Ops o restaurante está fechado!",
+    //         duration: 3000,
+    //         close: true,
+    //         gravity: "top", // `top` or `bottom`
+    //         position: "right", // `left`, `center` or `right`
+    //         stopOnFocus: true, // Prevents dismissing of toast en hover
+    //         style: {
+    //             background: "#ef4444",
+    //         },            
+    //     }).showToast();
+    //     return;
+    //  }
+
     if(cart.length === 0) return;
     if(addressInput.value === ""){
         addressWarn.classList.remove("hidden")
         addressInput.classList.add("border-red-500")
+        return;
     }  
 
+    //Enviar o pedido para API WhatsApp
+    const cartItems = cart.map((item) => {
+        return(
+            ` ${item.name} | Qtd: (${item.quantity}) | Preço: R$ ${item.price} |`   
+        )                 
+    }).join("")
+
+    const message = encodeURIComponent(cartItems)
+    const phone = "35998832330"
+
+    window.open(`https://wa.me/${phone}?text=${message} Endereço: ${addressInput.value}`, "_blank")
+
+    cart = [];
+    updateCartModal();
 
 })
 
 
+// Verificar a hora e manipular o card do horário
+function checkRestaurantOpen(){
+    const data = new Date();
+    const hora = data.getHours();
+    return hora >= 19 && hora < 23;
+    // true = restaurante está aberto
+}
+
+
+const spanItem = document.getElementById("date-span")
+const isOpen = checkRestaurantOpen();
+
+if(isOpen){
+    spanItem.classList.remove("bg-red-500");
+    spanItem.classList.add("bg-green-600")
+}else{
+    spanItem.classList.remove("bg-green-600")
+    spanItem.classList.add("bg-red-500")
+}
