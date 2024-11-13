f019177028fc275e9dfca5ec5a35be29777a1320
andsilva515 <anderson.ssilva@outlook.com>
2024-11-12 23:27:24 -0300

    navegação do menu card modal

--- scripts/script.js
+++ scripts/script.js
@@ -222,3 +222,34 @@ if(isOpen){
     spanItem.classList.remove("bg-green-600")
     spanItem.classList.add("bg-red-500")
 }
+
+
+// Menu Cart Abas
+document.addEventListener("DOMContentLoaded", function() {
+    const tabButtons = document.querySelectorAll(".tab-btn");
+    const tabContents = document.querySelectorAll(".tab-content");
+
+    tabButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            const targetTab = button.getAttribute("data-tab");
+
+            // Esconder todo o conteúdo das abas
+            tabContents.forEach(content => content.classList.add("hidden"));
+
+            // Mostrar o conteúdo da aba selecionada
+            document.getElementById(targetTab).classList.remove("hidden");
+
+            // Alterar a borda ativa do botão
+            tabButtons.forEach(btn => btn.classList.remove("border-black"));
+            button.classList.add("border-black");
+        });
+    });
+
+    // Abrir o modal
+   // document.getElementById("cart-modal").classList.remove("hidden");
+
+    // Fechar o modal
+    document.getElementById("close-modal-btn").addEventListener("click", () => {
+        document.getElementById("cart-modal").classList.add("hidden");
+    });
+});
