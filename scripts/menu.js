bc386e42c7984db1b832b66bf0fef29dcb290e18
andsilva515 <anderson.ssilva@outlook.com>
2025-02-02 17:53:39 -0300

    atualização preços

--- scripts/menu.js
+++ scripts/menu.js
@@ -14,136 +14,136 @@ const menuItems = [
   {
     name: "03 - Come-Come Egg",
     description: "Pão, hamburguer, mussarela, ovo, batata palha, maionese e molho especial.",
-    price: "12.00",
+    price: "14.00",
     imgSrc: "./assets/hamb-3.png",
-  },
+  },  
   
   {
     name: "04 - Come-Come Salada",
     description: "Pão, hamburguer, mussarela, alface, tomate, batata palha e molho especial.",
-    price: "14.00",
+    price: "15.00",
     imgSrc: "./assets/hamb-4.png",
   },
 
   {
     name: "05 - Come-Come Salada Egg",
     description: "Pão, hamburguer, mussarela, alface, tomate, ovo, batata palha e molho especial.",
-    price: "15.00",
+    price: "16.00",
     imgSrc: "./assets/hamb-5.png",
   },
 
   {
     name: "06 - Come-Come Bacon",
     description: "Pão, hamburguer, mussarela, bacon, alface, tomate, batata palha e molho especial.",
-    price: "25.00",
+    price: "28.00",
     imgSrc: "./assets/hamb-6.png",
   },
 
   {
     name: "07 - Come-Come Bacon Egg",
     description: "Pão, hamburguer, mussarela, bacon, alface, tomate, ovo, batata palha e molho especial.",
-    price: "26.00",
+    price: "30.00",
     imgSrc: "./assets/hamb-7.png",
   },
 
   {
-    name: "08 - Come-Come Tudo, Duplo, Mega",
-    description: "Pão, hambúrguer, mussarela, batata palha, alface, tomate, ovo, bacon, calabresa, pernil, milho, presunto, frango e molho especial.",
-    price: "25.00",
+    name: "08 - Come-Come Tudo",
+    description: "Pão, hambúrguer, mussarela, batata palha, alface, tomate, ovo, bacon, calabresa, lombo, milho, presunto, frango e molho especial.",
+    price: "30.00",
     imgSrc: "./assets/hamb-1.png",
   },
 
   {
     name: "09 - Come-Come Calabresa",
     description: "Pão, calabressa, mussarela, alface, tomate, batata palha e molho especial.",
-    price: "23.00",
+    price: "24.00",
     imgSrc: "./assets/hamb-2.png",
   },  
 
   {
       name: "10 - Come-Come Calabresa Egg",
       description: "Pão, Calabresa, mussarela, alface, tomate, ovo, batata palha e molho especial.",
-      price: "24.00",
+      price: "26.00",
       imgSrc: "./assets/hamb-3.png",
     },
     {
       name: "11 - Come-Come Lombo",
       description: "Pão, lombo, mussarela, batata palha, e molho especial.",
-      price: "24.00",
+      price: "26.00",
       imgSrc: "./assets/hamb-4.png",
     },
     {
       name: "12 - Come-Come Lombo c/ Maionese",
       description: "Pão, lombo, mussarela, batata palha, e maionese.",
-      price: "24.00",
+      price: "26.00",
       imgSrc: "./assets/hamb-5.png",
     },
     
     {
       name: "13 - Come-Come Lombo Egg",
       description: "Pão, lombo, mussarela, ovo, batata palha, e moho especial.",
-      price: "25.00",
+      price: "28.00",
       imgSrc: "./assets/hamb-6.png",
     },
 
     {
       name: "14 - Come-Come Lombo Salada",
       description: "Pão, lombo, mussarela, alface, tomate, batata palha, e moho especial.",
-      price: "25.00",
+      price: "26.00",
       imgSrc: "./assets/hamb-7.png",
     },
 
     {
       name: "15 - Come-Come Lombo Salada Egg",
       description: "Pão, lombo, mussarela, alface, tomate, ovo, batata palha, e moho especial.",
-      price: "26.00",
+      price: "30.00",
       imgSrc: "./assets/hamb-1.png",
     },
 
     {
       name: "16 - Come-Come Lombo Bacon",
       description: "Pão, lombo, mussarela, bacon, alface, tomate, batata palha, e moho especial.",
-      price: "30.00",
+      price: "32.00",
       imgSrc: "./assets/hamb-2.png",
     },
 
     {
       name: "17 - Come-Come Lombo Bacon Egg",
       description: "Pão, lombo, mussarela, bacon, alface, tomate, ovo, batata palha, e moho especial.",
-      price: "30.00",
+      price: "34.00",
       imgSrc: "./assets/hamb-3.png",
     },
 
     {
       name: "18 - Come-Come Lombo Especial",
       description: "Pão, lombo, mussarela, bacon, alface, tomate, ovo, milho, presunto, calabresa, batata palha, e moho especial.",
-      price: "32.00",
+      price: "35.00",
       imgSrc: "./assets/hamb-4.png",
     }, 
 
     {
       name: "19 - Come-Come Frango",
       description: "Pão, frango, mussarela, batata palha, e molho especial.",
-      price: "25.00",
+      price: "26.00",
       imgSrc: "./assets/hamb-5.png",
     },
     {
       name: "20 - Come-Come Frango Bacon",
       description: "Pão, frango, mussarela, bacon, alface, tomate, batata palha, e molho especial.",
-      price: "26.00",
+      price: "30.00",
       imgSrc: "./assets/hamb-6.png",
     },
     {
       name: "21 - Come-Come Frango c/ Maionese",
       description: "Pão, frango, mussarela, batata palha, e maionese.",
-      price: "25.00",
+      price: "26.00",
       imgSrc: "./assets/hamb-7.png",
     },
     
     {
       name: "22 - Come-Come Frango Egg",
       description: "Pão, frango, mussarela, ovo, batata palha, e molho especial.",
-      price: "26.00",
+      price: "28.00",
       imgSrc: "./assets/hamb-1.png",
     },
 
@@ -157,21 +157,21 @@ const menuItems = [
     {
       name: "24 - Come-Come Frango Salada Egg",
       description: "Pão, frango, mussarela, alface, tomate, ovo, batata palha, e molho especial.",
-      price: "27.00",
+      price: "28.00",
       imgSrc: "./assets/hamb-3.png",
     },
 
     {
       name: "25 - Come-Come Frango Bacon Egg",
       description: "Pão, frango, mussarela, bacon, alface, tomate, batata palha, e molho especial.",
-      price: "30.00",
+      price: "32.00",
       imgSrc: "./assets/hamb-4.png",
     },
 
     {
-      name: "26 - Come-Come Tudo Especial",
+      name: "26 - Come-Come Tudo Duplo",
       description: "Pão, frango, mussarela, batata palha, bacon, alface, tomate, ovo, milho, presunto, calabresa, e molho especial.",
-      price: "30.00",
+      price: "35.00",
       imgSrc: "./assets/hamb-5.png",
     },
 
@@ -191,34 +191,34 @@ const menuItems = [
     {
       name: "29 - Come-Come Filé Mignon Acebolado",
       description: "Pão, filé mignon, cebola, mussarela, tomate, batata palha, e molho especial.",
-      price: "",
+      price: "45.00",
       imgSrc: "./assets/hamb-1.png",
     },
     {
       name: "30 - Come-Come Lombo Acebolado",
       description: "Pão, lombo, cebola, mussarela, tomate e molho especial.",
-      price: "26.00",
+      price: "28.00",
       imgSrc: "./assets/hamb-2.png",
     },
     
     {
       name: "31 - Come-Come Lombo c/ Abacaxi",
       description: "Pão, lombo, abacaxi, batata palha, e molho especial.",
-      price: "28.00",
+      price: "30.00",
       imgSrc: "./assets/hamb-3.png",
     },
 
     {
       name: "32 - Come-Come Banana",
       description: "Pão, hambúrger, banana, milho, mussarela, batata palha, e molho especial.",
-      price: "15.00",
+      price: "18.00",
       imgSrc: "./assets/hamb-4.png",
     },
 
     {
       name: "33 - Come-Come Banana c/ Queijo",
       description: "Pão, hambúrger, banana, mussarela, milho.",
-      price: "17.00",
+      price: "20.00",
       imgSrc: "./assets/hamb-5.png",
     },
 
@@ -232,21 +232,21 @@ const menuItems = [
     {
       name: "35 - Come-Come Chedda Burger",
       description: "Pão, maionese, hambúrger, cheddar, batata palha, e molho especial.",
-      price: "18.00",
+      price: "20.00",
       imgSrc: "./assets/hamb-7.png",
     },
 
     {
       name: "36 - Come-Come Salada Especial",
       description: "Pão, maionese, 2 hambúrger, salada, cheddar, batata palha, e molho especial.",
-      price: "25.00",
+      price: "26.00",
       imgSrc: "./assets/hamb-1.png",
     }, 
 
     {
-      name: "37 - Come-Come",
-      description: "Pão, frango, 2 presuntos, 2 alface, 2 tomate, 2 bacon, 2 calebresa, 2 mussarela, 2 milhos, 2 ovos, hambúrger, batata palha e molho especial.",
-      price: "35.00",
+      name: "37 - Come-Come Tudo Mega",
+      description: "Pão, frango, 2 presunto, lombo, 2 alface, 2 tomate, 2 bacon, 2 calebresa, 2 mussarela, 2 milhos, 2 ovos, hambúrger, batata palha e molho especial.",
+      price: "40.00",
       imgSrc: "./assets/hamb-2.png",
     },
    
