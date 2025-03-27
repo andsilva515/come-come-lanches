a0adccc3d90301ce4b43e4fc521a90fb9040e63d
andsilva515 <anderson.ssilva@outlook.com>
2025-03-27 16:35:54 -0300

    horario de funcionamento

--- scripts/script.js
+++ scripts/script.js
@@ -201,7 +201,7 @@ checkoutBtn.addEventListener("click", function(){
 function checkRestaurantOpen(){
     const data = new Date();
     const hora = data.getHours();
-    return hora >= 10 && hora < 23;
+    return hora >= 19 && hora < 23;
     // true = restaurante está aberto
 }
 
